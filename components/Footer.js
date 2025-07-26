a0c76861c7bfba2a192d0bd87afa0b3c0deab29a
Ichha Dwivedi <ichhadwivedi2004@gmail.com>
2025-07-26 16:32:07 +0000

    responsive

--- components/Footer.js
+++ components/Footer.js
@@ -4,17 +4,16 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-   <footer className="bg-gray-900 text-white w-full px-4 py-3 flex flex-col items-center space-y-1 text-sm">
-  <p className="text-center">
-    &copy; {new Date().getFullYear()} Get me A Chai - All rights reserved!
-  </p>
-  <p className="text-center text-xs flex items-center gap-1">
-    Made with
-    <img src="/heart_icon.svg" alt="heart" className="w-4 h-4 inline-block" />
-    by ID
-  </p>
-</footer>
-
+    <footer className="bg-gray-900 text-white fixed bottom-0 left-0 w-full px-4 py-3 flex flex-col items-center space-y-1 text-sm z-50">
+      <p className="text-center">
+        &copy; {currentYear} Get me A Chai - All rights reserved!
+      </p>
+      <p className="text-center text-xs flex items-center gap-1">
+        Made with
+        <img src="/heart_icon.svg" alt="heart" className="w-4 h-4 inline-block invert" />
+        by ID
+      </p>
+    </footer>
   )
 }
 
