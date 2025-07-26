731630a51013933c1d97df36bdccd6d790e2ef50
Ichha Dwivedi <ichhadwivedi2004@gmail.com>
2025-07-26 13:34:54 +0000

    updating route of success

--- app/success/page.js
+++ app/success/page.js
@@ -5,10 +5,10 @@ export default function SuccessPage({ searchParams }) {
       <h1 className="text-3xl font-bold text-green-600">Payment Successful!</h1>
       <p className="mt-4">Thank you for supporting {username}!</p>
       <a
-        href="/"
+        href={`/${username}`}
         className="mt-8 inline-block px-6 py-2 bg-green-600 text-white rounded-lg font-semibold hover:bg-green-700 transition"
       >
-        Go Back Home
+        Go Back to your page
       </a>
     </div>
   );
